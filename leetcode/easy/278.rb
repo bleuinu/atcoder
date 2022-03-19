49a37a5f300e00553528c2bc0f3a4abfa52dd90d
Blue Eu <eu.blue@pm.me>
2022-03-19 20:11:01 +0900

    Leetcode algorithm challenge day1

--- leetcode/easy/278.rb
+++
@@ -1,5 +0,0 @@
-# In find-minimum mode, method bsearch returns the first element for which the block returns true.
-# so it will find the first bad version which returns true
-def first_bad_version(n)
-  (1..n).bsearch { |i| is_bad_version(i) }
-end
